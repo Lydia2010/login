52c768ff09e9eea852198dccf0fece80c19bff7f
Lydia2010 <lidiya.s@gmail.com>
2019-06-16 14:13:58 -0400

    token was been added

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,26 @@
 let express = require('express');
 let router = express.Router();
 let rando = require('random-words') // added package
+let jwt = require('jsonwebtoken');
+require('dotenv').config(); // load environment variable
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
 router.get('/word', (req, res) => {
-  let word = rando(1);
-  res.send({word: word});
+  let token = req.query.token;
+  try {
+    jwt.verify(token, process.env.SECRET);
+    let word = rando(1);
+    res.send({word: word});
+  } catch(err){
+    res.send({error: "invalid token", details: err})
+  }
+
+
 });
 
 router.get('/words/:count', (req, res) => {
@@ -34,6 +45,27 @@ router.post('/word',(req,res) => {
   res.send({word: "Funny"})
 });
 
+router.get('/secret', (req, res) => {
+  res.send({secret: process.env.SECRET})
+});
+
+// create login
+
+router.post('/login', (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  //Verify username and password
+  if(username === 'fred' && password === process.env.PASSWORD) {
+    let token = jwt.sign({
+      username: username},
+        process.env.SECRET,
+        {expiresIn: '10 minutes'
+    });
+    res.send({token: token})
+  }else {
+    res.send({error: "Invalid login credentials"})
+  }
+});
 
 
 module.exports = router;
